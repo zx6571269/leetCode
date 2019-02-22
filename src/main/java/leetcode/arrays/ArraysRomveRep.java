4a800d48443c1ac45507b5fbc4f9fb8e347c178b
564994247@qq.com <564994247@qq.com>
2019-02-22 18:05:54 +0800

    移除数组重复发元素

--- src/main/java/leetcode/arrays/ArraysRomveRep.java
+++ src/main/java/leetcode/arrays/ArraysRomveRep.java
@@ -1,7 +1,22 @@
 package leetcode.arrays;
 
 /**
- * Created by Administrator on 2018/10/18.
+ * . 删除排序数组中的重复项
+ *
+ * 给定数组 nums = [1,1,2],
+
+ 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+
+ 你不需要考虑数组中超出新长度后面的元素。
+
+ 示例 2:
+
+ 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+
+ 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+
+ 你不需要考虑数组中超出新长度后面的元素。
+
  */
 public class ArraysRomveRep {
 
@@ -13,8 +28,10 @@ public class ArraysRomveRep {
         for (int i = 0; i < n; ++i) {
             System.out.println("i:"+nums[i]);
             System.out.println("j:"+nums[j]);
-            if (nums[i] != nums[j])
+            if (nums[i] != nums[j]){
                 nums[++j] = nums[i];
+            }
+
         }
 
         System.out.println(j+1);
