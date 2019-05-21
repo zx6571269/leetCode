6610f37ba858d8f96593e09529dc23358832f98a
zengxin <564994247@qq.com>
2019-05-21 23:07:23 +0800

    DateUtils增加

---
+++ src/main/java/zx/untils/DateUntils.java
@@ -0,0 +1,66 @@
+package zx.untils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class DateUntils {
+
+    private  static ThreadLocal<DateFormat> threadLocal = new ThreadLocal<DateFormat>(){
+        @Override
+        protected DateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        }
+    };
+
+    /**
+     *  将 str
+     * @param dateStr
+     * @return
+     * @throws ParseException
+     */
+    public static Date parse(String dateStr) throws ParseException {
+        return threadLocal.get().parse(dateStr);
+    }
+
+    /**
+     *  将 Date 转换为 yyyy-MM-dd
+     * @param date
+     * @return yyyy-MM-dd
+     */
+    public static String formatDay(Date date){
+        return threadLocal.get().format(date).substring(0,9);
+    }
+
+    /**
+     *  将 Date 转换为 yyyy-MM-dd HH:mm:ss
+     * @param date
+     * @return yyyy-MM-dd HH:mm:ss
+     */
+    public static  String formartSecond(Date date){
+        return threadLocal.get().format(date);
+    }
+
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(100);
+        for (int i = 0; i <  100; i++) {
+            service.execute(()->{
+
+                try {
+                    System.out.println(parse("2018-01-02 09:45:59"));
+                    System.out.println(formatDay(new Date()));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+            });
+        }
+    }
+
+
+
+}
